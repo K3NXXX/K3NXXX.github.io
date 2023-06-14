3cafd905211ce821f0eeaceb855fc7ec204eb80d
Andrushevskyi Volodymyr <116910392+K3NXXX@users.noreply.github.com>
2023-06-14 22:38:41 +0300

    Delete fitmirror-site directory

--- fitmirror-site/script.js
+++
@@ -1,5 +0,0 @@
-const menuBtn = document.querySelector(".menu_btn");
-const headerMenu = document.querySelector(".header__phone-menu");
-menuBtn.addEventListener("click", () => {
-    headerMenu.classList.toggle("open--menu")
-})
